f6633a411e88bdf93379020c55ee20f6a19f2d24
Andrew Bergheim <andrewbergheim@gmail.com>
2020-01-21 10:17:21 -0700

    renamed classes, defined variables

---
+++ app.js
@@ -0,0 +1,19 @@
+// dependencies
+const inquirer = require("inquirer")
+const fs = require("fs")
+const classes = require("./lib/class") 
+// blank array of team members to use with building HTML
+let teamArray = [];
+//blank html string to be added to later
+let usefulHTML = "";
+//inquirer prompt definition
+let employeePrompt = inquirer.prompt([
+
+]).then(function(answer){
+
+})
+
+//html generation loop
+let htmlGen;
+//write html to file
+let writeHTML;
