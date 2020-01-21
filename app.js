98b87b15afc761575c9b4150fb7e4c7ed4a0dd6c
Andrew Bergheim <andrewbergheim@gmail.com>
2020-01-21 13:15:18 -0700

    Update app.js
    functional branch logic and looping on CLI

--- app.js
+++ app.js
@@ -1,17 +1,191 @@
 // dependencies
 const inquirer = require("inquirer")
 const fs = require("fs")
-const classes = require("./lib/class") 
+const classes = require("./lib/classes")
+let Employee = classes.employee;
+let Manager = classes.manager;
+let Engineer = classes.engineer;
+let Intern = classes.intern;
+
 // blank array of team members to use with building HTML
 let teamArray = [];
 //blank html string to be added to later
 let usefulHTML = "";
-//inquirer prompt definition
-let employeePrompt = inquirer.prompt([
 
-]).then(function(answer){
+//inquirer question chains 
 
+// default question asked first
+defaultQuestion = [
+    {
+        message: "What type of team member would you like to add?",
+        type: "list",
+        name: "profession",
+        choices: ["intern","employee","manager","engineer"]      
+    }
+]
+
+// branch logic asks question for correct type of employee
+employeeQuestions = [
+    {   
+        message:"What is the employee's name?",
+        name:"name",
+        type:"input"
+    },
+    
+    {
+        message:"What is the employee's email?",
+        name:"email",
+        type:"input"
+    },
+    
+    {
+        message:"What is the employee's ID?",
+        name:"id",
+        type:"input"
+    },
+    {
+        message: "Would you like to continue adding employees?",
+        type: "list",
+        name: "continue",
+        choices: ["yes","no"]      
+    }
+    
+]
+
+managerQuestions = [
+    {   
+        message:"What is the manager's name?",
+        name:"name",
+        type:"input"
+    },
+    
+    {
+        message:"What is the manager's office number?",
+        name:"officeNumber",
+        type:"input"
+    },
+    
+    {
+        message:"What is the manager's ID?",
+        name:"id",
+        type:"input"
+    },
+
+    {
+        message: "What is the manager's email?",
+        name:"email",
+        type: "imput"
+    },
+    {
+        message: "Would you like to continue adding employees?",
+        type: "list",
+        name: "continue",
+        choices: ["yes","no"]      
+    }
+]
+
+internQuestions = [
+    {   
+        message:"What is the intern's name?",
+        name:"name",
+        type:"input"
+    },
+    
+    {
+        message:"What school does the intern attend?",
+        name:"school",
+        type:"input"
+    },
+    
+    {
+        message:"What is the employee's ID?",
+        name:"id",
+        type:"input"
+    },
+
+    {
+        message: "What is the intern's email?",
+        name:"email",
+        type: "imput"
+    },
+    {
+        message: "Would you like to continue adding employees?",
+        type: "list",
+        name: "continue",
+        choices: ["yes","no"]      
+    }
+]
+
+engineerQuestions = [
+    {   
+        message:"What is the engineer's name?",
+        name:"name",
+        type:"input"
+    },
+    
+    {
+        message:"What is the engineer's gitHub?",
+        name:"github",
+        type:"input"
+    },
+    
+    {
+        message:"What is the engineer's ID?",
+        name:"id",
+        type:"input"
+    },
+
+    {
+        message: "What is the engineer's email?",
+        name:"email",
+        type: "imput"
+    },
+    {
+        message: "Would you like to continue adding employees?",
+        type: "list",
+        name: "continue",
+        choices: ["yes","no"]      
+    }
+]
+
+//inquirer prompt
+let employeePrompt = function(){ inquirer.prompt(defaultQuestion)
+.then(function(response){
+    // question set logic & object creation
+    if (response.profession == "intern"){
+        inquirer.prompt(internQuestions).then(function(res){
+            teamArray.push(new Intern(res.name, res.id, res.email, res.school))
+            if (res.continue == "yes"){
+                employeePrompt()
+            }
+    })}
+
+    else if (response.profession == "manager"){
+        inquirer.prompt(managerQuestions).then(function(res){
+            teamArray.push(new Manager(res.name, res.id, res.email, res.officeNumber))
+            if (res.continue == "yes"){
+                employeePrompt()
+            }
+    })}
+
+    else if (response.profession == "engineer"){
+        inquirer.prompt(engineerQuestions).then(function(res){
+            teamArray.push(new Engineer(res.name, res.id, res.email, res.github))
+            if (res.continue == "yes"){
+                employeePrompt()
+            }
+    })}
+
+    else{
+        inquirer.prompt(employeeQuestions).then(function(res){
+            teamArray.push(new Employee(res.name, res.id, res.email))
+            if (res.continue == "yes"){
+                employeePrompt()
+            }
+    })}   
 })
+}
+employeePrompt()
 
 //html generation loop
 let htmlGen;
